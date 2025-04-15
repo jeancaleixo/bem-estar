f846898b07cbcf6647356f3ebcdce250804d4e88
Jean De Castro <jean.castroaleixo@gmail.com>
2025-04-14 23:03:22 -0300

    feat: add user model

---
+++ app/src/interfaces/userInteface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Interfaces;
+
+interface userInteface
+{
+    public function getId(): int;
+    public function getName(): string;
+    public function getEmail(): string;
+    public function getPassword(): string;
+
+    public function setId(int $id): void;
+    public function setName(string $name): void;
+    public function setEmail(string $email): void;
+    public function setPassword(string $password): void;
+}
---
+++ app/src/models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use App\Interfaces\userInteface;
+
+class User implements userInteface
+{
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $password;
+
+    public function __construct(int $id, string $name, string $email, string $password)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+    }
+}
