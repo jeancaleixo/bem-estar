88a21323396043de6d5a7487daf7f5dd6311da12
Jean De Castro <jean.castroaleixo@gmail.com>
2025-04-24 22:48:38 -0300

    feat: reformulacao do projeto

---
+++ App/index.php
@@ -0,0 +1,26 @@
+<!-- login -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Assets/css/style.css">
+    <title>Bem-Estar</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <form class="form" action="">
+                <label for="">Digite seu Email aqui!</label>
+                <input type="text" placeholder="Email">
+                <label for="">Digite sua Senha aqui!</label>
+                <input type="text" placeholder="email">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
